d3959bfb15a376cb08b26531f34b1e0a0e0660a8
Tobin Tojo <69814148+TobinTojo@users.noreply.github.com>
2022-09-25 13:11:08 -0400

    Character Movement in 3D Environment
    Created a character controller for 3D player.
    Added platforms such as moving platforms and idle platforms

---
+++ 3DGame/Assets/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{   [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float jumpForce = 5f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;
+    [SerializeField] Animator anim;
+    Rigidbody rb;
+    [SerializeField] AudioSource source;
+    [SerializeField] AudioClip jump;
+    public bool isJump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -15f) {
+            transform.position = new Vector3(0f, 0.55f, 0f);
+        }
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
+        if (movement != Vector3.zero) {
+            Quaternion targetRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 360 * Time.deltaTime);
+            anim.SetBool("isRunning", true);
+        }
+        else {
+            anim.SetBool("isRunning", false);
+        }
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+
+        if (Input.GetButtonDown("Jump") && Physics.CheckSphere(groundCheck.position, .1f, ground) == true) {
+            source.clip = jump;
+            source.Play();
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            anim.SetBool("isJumping", true);
+            isJump = true;
+        }
+        if (Physics.CheckSphere(groundCheck.position, 0.1f, ground) == true) {
+            anim.SetBool("isJumping", false);
+            isJump = false;
+        }
+        else {
+            anim.SetBool("isJumping", true);
+            
+        }
+    }
+}
---
+++ 3DGame/Assets/WayPointFollower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointFollower : MonoBehaviour
+{
+    [SerializeField] GameObject[]  wayPoints;
+    int currentWayPointIndex = 0;
+
+    [SerializeField] float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, wayPoints[currentWayPointIndex].transform.position) < 0.1f)
+        {
+            currentWayPointIndex++;
+            if (currentWayPointIndex >= wayPoints.Length)
+                currentWayPointIndex = 0;
+        } 
+        transform.position = Vector3.MoveTowards(transform.position, wayPoints[currentWayPointIndex].transform.position, speed * Time.deltaTime);
+
+    }
+}
