a076f275fc4c80ce68d0716a5b1d56008a084663
Tobin Tojo <69814148+TobinTojo@users.noreply.github.com>
2022-09-29 01:45:47 -0400

    Implemented another collectable in the game
    - Red rings update

---
+++ 3DGame/Assets/redRing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class redRing : MonoBehaviour
+{
+    [SerializeField] AudioClip ring;
+    [SerializeField] AudioSource source;
+    [SerializeField] MeshRenderer mesh;
+    [SerializeField] MeshRenderer mesh2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void destroyObject() {
+        Destroy(this.gameObject);
+    }
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.name == "Player") {
+            mesh.enabled = false;
+            mesh2.enabled = false;
+            source.clip = ring;
+            source.Play();
+            Invoke("destroyObject", 0.8f);
+        }
+    }
+}
